680b0e5847182ce6d2cc73922ac15fa8c2767481
Talha86-ux <rjtalha555@gmail.com>
2024-02-26 03:45:16 +0500

    Setup mutations and create a user mutation

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,13 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :email, String, null: true
+    field :posts, [Types::PostType], null: true
+    field :postCount, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def postCount
+      object.posts.count
+    end
   end
 end
